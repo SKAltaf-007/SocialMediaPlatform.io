4b7154d4784c3ebedd43059ec735d9b7b3653722
SKAltaf-007 <138595208+SKAltaf-007@users.noreply.github.com>
2023-07-31 16:47:57 +0530

    Add files via upload

---
+++ demo/db_conn.php
@@ -0,0 +1,20 @@
+<?php
+
+$sname= "localhost";
+
+$unmae= "root";
+
+$password = "";
+
+$db_name = "database";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+
+    echo "Connection failed!";
+
+}
+else{
+    echo "connected to database";
+}
---
+++ demo/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>GFG- Store Data</title>
+   </head>
+   <body>
+      <center>
+         <h1>Storing Form data in Database</h1>
+         <form action="login.php" method="post">
+             
+<p>
+               <label for="user_name">username:</label>
+               <input type="text" name="user_name" id="user_name">
+            </p>
+ 
+             
+<p>
+               <label for="Password">Last Name:</label>
+               <input type="text" name="password" id="Password">
+            </p>
+ 
+             
+
+ 
+            <input type="submit" value="Submit">
+         </form>
+      </center>
+   </body>
+</html>
